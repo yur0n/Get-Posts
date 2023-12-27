e8af3d9c29dad9a8f948500b06fa37a9fb706f7e
yur0n <86810041+yur0n@users.noreply.github.com>
2023-12-27 06:43:47 +0200

    Update connection.js

--- bot/database/connection.js
+++ bot/database/connection.js
@@ -3,6 +3,11 @@ import env from "../../env/env.js"
 
 mongoose.set('strictQuery', false)
 
-export default mongoose.createConnection(env.DB_CONNECTION)
+export default mongoose.createConnection(env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true,
+    //useFindAndModify: false
+})
 
 mongoose.connect(env.DB_CONNECTION)
